ca65ab2d8773d76ccb1f9d3196b768514f98dd71
Leonty Chudinov <leonty@inbox.ru>
2021-08-13 08:03:02 +0500

    maxargs

--- 1-introduction/ts/src/index.ts
+++ 1-introduction/ts/src/index.ts
@@ -1,3 +1,5 @@
 import { prog1 } from "./prog/prog1";
+import { maxargs } from './maxargs';
 
 console.log(JSON.stringify(prog1, null, 2));
+console.log(`maxargs = ${maxargs(prog1)}`);
--- 1-introduction/ts/src/maxargs.ts
+++ 1-introduction/ts/src/maxargs.ts
@@ -1,12 +1,38 @@
-import { CompoundStatement } from './types/statement';
-import { Statement } from './types';
+import { ExpressionPair, EseqExpression } from './types/expression';
+import { CompoundStatement, PrintStatement, AssignStatement } from './types/statement';
+import { ExpressionList, Statement, Expression } from './types';
+
 export function maxargs(st: Statement): number {
   switch (st.kind) {
     case 'AssignStatement':
-      
+      const assign = st as AssignStatement;
+      return maxargs_exp(assign.expression);
     case 'CompoundStatement':
-      const compound = st.statement as CompoundStatement;
+      const compound = st as CompoundStatement;
       return Math.max(maxargs(compound.st1), maxargs(compound.st2));
     case 'PrintStatement':
+      const print = st as PrintStatement;
+      return maxargs_exps(print.exps);
+  }
+}
+
+function maxargs_exps(exps: ExpressionList): number {
+  if (exps.kind === 'pair') {
+    return 1 + maxargs_exps((exps as ExpressionPair).tail);
+  } else {
+    return 1;
+  }
+}
+
+function maxargs_exp(exp: Expression): number {
+  switch (exp.kind) {
+    case 'IdExpression':
+    case 'NumExpression':
+      return 1;
+    case 'OpExpression':
+      return 0;
+    case 'EseqExpression':
+      const eseq = exp as EseqExpression;
+      return Math.max(maxargs(eseq.statement), maxargs_exp(eseq.expression));
   }
 }
